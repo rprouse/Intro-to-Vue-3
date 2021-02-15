c801e1467c4f2d3d8e7b4ebac286df3fd5265f0c
Rob Prouse <rob@prouse.org>
2021-02-15 16:16:11 -0500

    Add a reviews component

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -5,9 +5,9 @@ app.component('product-display', {
       required: true
     }
   },
-  template: 
-  /*html*/
-  `<div class="product-display">
+  template:
+  /*html*/`
+  <div class="product-display">
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
@@ -23,18 +23,18 @@ app.component('product-display', {
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
 
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.id"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
           :style="{ backgroundColor: variant.color }">
         </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
+
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          :disabled="!inStock"
           v-on:click="addToCart">
           Add to Cart
         </button>
--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: [],
-            premium: true
-        }
-    },
-    methods: {
-        updateCart(id) {
-            this.cart.push(id)
-        }
+  data() {
+    return {
+      cart: [],
+      reviews: [],
+      premium: true
     }
-  })
-  
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    addReview(review) {
+      this.reviews.push(review);
+    }
+  }
+})
